63bb787903244719d87ceaa2d121fd9dd4272bfb
Romanov Nikita <rominikhom@gmail.com>
2025-07-26 19:55:17 +0400

    Обновил requirements

--- conftest.py
+++ conftest.py
@@ -15,8 +15,7 @@ def setup_browser(request):
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
-        },
-        "goog:loggingPrefs": {"browser": "ALL"}
+        }
     }
     options.capabilities.update(selenoid_capabilities)
 
