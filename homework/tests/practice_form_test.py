79b1d2d89f850a47616319d64d427667a4a5c52c
Romanov Nikita <rominikhom@gmail.com>
2025-07-26 17:11:47 +0400

    Добавил skip тест

--- homework/tests/practice_form_test.py
+++ homework/tests/practice_form_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from homework.demoqa_tests.model.pages.registration_page import RegistrationPage
 
 registration_page = RegistrationPage()
@@ -152,6 +154,37 @@ def test_submit_form_failed():
         'NCR Delhi'
     )
 
+def test_submit_form_failed2():
+    registration_page.open()
+    registration_page.set_name("Maria")
+    registration_page.set_last_name("Mironov")
+    registration_page.set_email("jaosnjgotnasgnon@asnfomnoas.weq")
+    registration_page.set_gender("Male")
+    registration_page.set_phone_number('0123456789')
+    registration_page.set_date_of_birth('1997', 'April', '11')
+    registration_page.set_subject('English')
+    registration_page.set_hobbies('Sports', 'Music')
+    registration_page.set_address('Russia, Moscow, st.Pushkina, h.23')
+    registration_page.upload_picture()
+    registration_page.set_state('NCR')
+    registration_page.set_city('Delhi')
+    registration_page.submit_form()
+
+    # Проверяем, что форма отправлена
+    registration_page.should_registration_user_data(
+        'Ivan Mironov',
+        'jaosnjgotnasgnon@asnfomnoas.weq',
+        'Male',
+        '0123456789',
+        '11 April,1997',
+        'English',
+        'Sports, Music',
+        'photo.jpg',
+        'Russia, Moscow, st.Pushkina, h.23',
+        'NCR Delhi'
+    )
+
+@pytest.mark.skip
 def test_submit_form_failed2():
     registration_page.open()
     registration_page.set_name("Maria")
